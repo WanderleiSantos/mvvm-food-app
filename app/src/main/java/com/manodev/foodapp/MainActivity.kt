58e9fc5cde112235c4b4631d69584358fcb863ca
Wanderlei Santos <wanderleidias.santos@hotmail.com>
2025-01-20 17:41:34 -0300

    feat: AuthScreen

--- app/src/main/java/com/manodev/foodapp/MainActivity.kt
+++ app/src/main/java/com/manodev/foodapp/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -16,6 +17,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.manodev.foodapp.ui.features.auth.AuthScreen
 import com.manodev.foodapp.ui.theme.MVVMFoodAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
@@ -64,12 +66,11 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MVVMFoodAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier =  Modifier.fillMaxSize()) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding))
+                    AuthScreen()
                 }
+
             }
         }
 
